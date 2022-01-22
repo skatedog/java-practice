1a43af44b9dc7c68ec9efb9f4614db74d3a4d61d
skatedog <skatedog0001@gmail.com>
2022-01-22 22:22:06 +0900

    [Finish]chapter4

---
+++ dwc-basic/Chapter04.java
@@ -0,0 +1,33 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		int num1;
+		String str1;
+		
+		num1 = 10;
+		str1 = "Hello World!";
+		
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20;
+		
+		System.out.println(num1);
+		
+		int num2 = 30;
+		System.out.println(num2);
+		
+		final int MIN_NUMBER = 10;
+		
+		System.out.println(MIN_NUMBER);
+		
+		String name = "hoge";
+		System.out.println(name);
+		
+		name = "fuga";
+		System.out.println(name);
+		
+		int num3 = 7;
+		System.out.println(num3);
+	}
+}
