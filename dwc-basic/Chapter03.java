99b5c8a96b4b5305a0d6827290a19e572df778b9
skatedog <skatedog0001@gmail.com>
2022-01-22 22:06:41 +0900

    [Finish]chapter3

---
+++ dwc-basic/Chapter03.java
@@ -0,0 +1,15 @@
+
+public class Chapter03 {
+	// コメント
+	
+	/*
+	 * 複数行の
+	 * コメント
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello WEBCAMP!");
+		System.out.println("Hello Java!");
+	}
+	
+}
